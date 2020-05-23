c062967ced1b23d13b6d30545c85cc8a756de08a
Jake Posner <jposner91@gmail.com>
2020-05-23 15:38:29 -0400

    Initial set up of project

--- Interval Training Timer/ViewController.swift
+++ Interval Training Timer/ViewController.swift
@@ -9,10 +9,124 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var activeLabel: UILabel!
+    @IBOutlet weak var restLabel: UILabel!
+    @IBOutlet weak var activeTimerValue: UILabel!
+    @IBOutlet weak var restTimerValue: UILabel!
+    @IBOutlet weak var cyclesRemainingLabel: UILabel!
+    @IBOutlet weak var workoutControlButton: UIButton!
+    
+    var activeTimerInitialValue = 5
+    var restTimerInitialValue = 2
+    var seconds = 5
+    var cyclesRemaining = 10
+    var timerState = "active"
+    var timer : Timer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        cyclesRemainingLabel.text = "cycles remaining: " + String(cyclesRemaining)
+    }
+    
+    @IBAction func buttonWasPressed(_ sender: Any) {
+        if workoutControlButton.titleLabel?.text == "start workout" {
+            startWorkout()
+        } else {
+            stopWorkout()
+        }
+    }
+    
+    func startWorkout() {
+        activeLabel.textColor = UIColor.green
+        restLabel.textColor = UIColor.white
+        activeTimerValue.text = String(activeTimerInitialValue)
+        restTimerValue.text = String(restTimerInitialValue)
+        
+        timer = Timer.scheduledTimer(
+            timeInterval: 1.0,
+            target: self, selector:
+            #selector(ViewController.activeCounter),
+            userInfo: nil,
+            repeats: true)
+        
+        workoutControlButton.setTitle("stop workout", for: .normal)
+        workoutControlButton.setTitleColor(UIColor.red, for: .normal)
+    }
+    
+    func stopWorkout() {
+        workoutControlButton.setTitle("start workout", for: .normal)
+        workoutControlButton.setTitleColor(UIColor.green, for: .normal)
+        resetTimers()
+        timer?.invalidate()
+        restLabel.font = restLabel.font.withSize(30)
+        restTimerValue.font = restTimerValue.font.withSize(60)
+        activeLabel.font = activeLabel.font.withSize(42)
+        activeTimerValue.font = activeTimerValue.font.withSize(100)
+        resetCyclesRemaining()
+        timerState = "active"
+    }
+    
+    @objc func activeCounter()
+    {
+        seconds -= 1
+        print(seconds)
+        if seconds == 0 {
+            resetTimers()
+            swapTimers()
+        } else {
+            if timerState == "active" {
+                activeTimerValue.text = String(seconds)
+            } else {
+                restTimerValue.text = String(seconds)
+            }
+        }
+    }
+    
+    func resetTimers() {
+        activeTimerValue.text = String(activeTimerInitialValue)
+        restTimerValue.text = String(restTimerInitialValue)
+        activeLabel.textColor = UIColor.white
+        restLabel.textColor = UIColor.white
+        seconds = activeTimerInitialValue
+    }
+    
+    func swapTimers() {
+        if timerState == "active" {
+            timerState = "rest"
+            seconds = restTimerInitialValue
+            activeLabel.textColor = UIColor.white
+            restLabel.textColor = UIColor.green
+            activeLabel.font = activeLabel.font.withSize(30)
+            activeTimerValue.font = activeTimerValue.font.withSize(60)
+            restLabel.font = restLabel.font.withSize(42)
+            restTimerValue.font = restTimerValue.font.withSize(100)
+        } else {
+            timerState = "active"
+            seconds = activeTimerInitialValue
+            restLabel.textColor = UIColor.white
+            activeLabel.textColor = UIColor.green
+            restLabel.font = restLabel.font.withSize(30)
+            restTimerValue.font = restTimerValue.font.withSize(60)
+            activeLabel.font = activeLabel.font.withSize(42)
+            activeTimerValue.font = activeTimerValue.font.withSize(100)
+            decrementCyclesRemaining()
+        }
+    }
+    
+    func decrementCyclesRemaining() {
+        cyclesRemaining -= 1
+        if cyclesRemaining > 0 {
+            cyclesRemainingLabel.text = "cycles remaining: " + String(cyclesRemaining)
+        } else {
+            stopWorkout()
+        }
+        
+    }
+    
+    func resetCyclesRemaining() {
+        cyclesRemaining = 10
+        cyclesRemainingLabel.text = "cycles remaining: " + String(cyclesRemaining)
     }
 
 
