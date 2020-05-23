6418a7fe1572803de47c8955a51db4b76625d27d
Jake Posner <jposner91@gmail.com>
2020-05-23 18:54:12 -0400

    the user can now toggle their own intervals with a minimum of 5 seconds for each interval. Rest can be up to 60 seconds and active can go up to 120 seconds so far.

--- Interval Training Timer/ViewController.swift
+++ Interval Training Timer/ViewController.swift
@@ -16,19 +16,34 @@ class ViewController: UIViewController {
     @IBOutlet weak var cyclesRemainingLabel: UILabel!
     @IBOutlet weak var workoutControlButton: UIButton!
     
+    @IBOutlet weak var restTimerPlusMinus: UIStepper!
+    @IBOutlet weak var activeTimerPlusMinus: UIStepper!
     var activeTimerInitialValue = 5
     var restTimerInitialValue = 2
-    var seconds = 5
+    var seconds = 30
     var cyclesRemaining = 10
     var timerState = "active"
     var timer : Timer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         cyclesRemainingLabel.text = "cycles remaining: " + String(cyclesRemaining)
     }
+    @IBAction func activeStepperChanged(_ sender: UIStepper) {
+        let newActiveTimerValueAsInt = Int(sender.value)
+        let newActiveTimerValueAsString = String(newActiveTimerValueAsInt)
+        print("value changed: " + newActiveTimerValueAsString)
+        activeTimerValue.text = newActiveTimerValueAsString
+        activeTimerInitialValue = newActiveTimerValueAsInt
+    }
     
+    @IBAction func restStepperChanged(_ sender: UIStepper) {
+        let newRestTimerValueAsInt = Int(sender.value)
+        let newRestTimerValueAsString = String(newRestTimerValueAsInt)
+        print("value changed: " + newRestTimerValueAsString)
+        restTimerValue.text = newRestTimerValueAsString
+        restTimerInitialValue = newRestTimerValueAsInt
+    }
     @IBAction func buttonWasPressed(_ sender: Any) {
         if workoutControlButton.titleLabel?.text == "start workout" {
             startWorkout()
@@ -40,8 +55,16 @@ class ViewController: UIViewController {
     func startWorkout() {
         activeLabel.textColor = UIColor.green
         restLabel.textColor = UIColor.white
+        
+        activeTimerPlusMinus.isHidden = true
+        restTimerPlusMinus.isHidden = true
+        
+        restLabel.font = restLabel.font.withSize(30)
+        restTimerValue.font = restTimerValue.font.withSize(60)
         activeTimerValue.text = String(activeTimerInitialValue)
         restTimerValue.text = String(restTimerInitialValue)
+        
+        seconds = activeTimerInitialValue
         
         timer = Timer.scheduledTimer(
             timeInterval: 1.0,
@@ -59,18 +82,19 @@ class ViewController: UIViewController {
         workoutControlButton.setTitleColor(UIColor.green, for: .normal)
         resetTimers()
         timer?.invalidate()
-        restLabel.font = restLabel.font.withSize(30)
-        restTimerValue.font = restTimerValue.font.withSize(60)
+        restLabel.font = restLabel.font.withSize(42)
+        restTimerValue.font = restTimerValue.font.withSize(100)
         activeLabel.font = activeLabel.font.withSize(42)
         activeTimerValue.font = activeTimerValue.font.withSize(100)
         resetCyclesRemaining()
         timerState = "active"
+        activeTimerPlusMinus.isHidden = false
+        restTimerPlusMinus.isHidden = false
     }
     
     @objc func activeCounter()
     {
         seconds -= 1
-        print(seconds)
         if seconds == 0 {
             resetTimers()
             swapTimers()
